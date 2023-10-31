39e767ca45c6557c93cb32b22b6f72d3b31dbdeb
veroyols <vero.scholz@gmail.com>
2023-10-31 19:55:43 -0300

    redondel

--- app/src/main/java/com/example/mascotasapp/navigation/fragments/SettingFragment.java
+++ app/src/main/java/com/example/mascotasapp/navigation/fragments/SettingFragment.java
@@ -2,7 +2,9 @@ package com.example.mascotasapp.navigation.fragments;
 
 import static android.content.Context.MODE_PRIVATE;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.Image;
 import android.os.Bundle;
@@ -19,23 +21,33 @@ import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
+import com.example.mascotasapp.LoginActivity;
 import com.example.mascotasapp.R;
+import com.example.mascotasapp.signup.SignUpActivity;
 import com.google.android.material.chip.Chip;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingFragment extends Fragment {
+    FirebaseAuth mAuth;
     ImageView image;
     Chip language;
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
+    Button saveButton;
+    Button signOutButton;
+
     private final String PREF_NAME = "userPref";
     private final int PRIVATE = 0;
     private final String KEY = "usuario";
     public SettingFragment() {}
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_setting, container, false);
+        //Auth
+        mAuth = FirebaseAuth.getInstance();
         //Editor de shared preference
         sharedPreferences = getContext().getSharedPreferences(PREF_NAME, PRIVATE);
         editor =  sharedPreferences.edit();
@@ -44,6 +56,15 @@ public class SettingFragment extends Fragment {
         image = view.findViewById(R.id.frag_setting_image);
         image.bringToFront();
         language = view.findViewById(R.id.frag_setting_language);
+
+        saveButton = view.findViewById(R.id.frag_setting_save);
+
+        signOutButton = view.findViewById(R.id.frag_setting_signOut);
+        signOutButton.setOnClickListener(v -> {
+            mAuth.signOut();
+            Intent intent = new Intent(requireContext(), LoginActivity.class);
+            startActivity(intent);
+        });
 
         return view;
     }
@@ -57,4 +78,5 @@ public class SettingFragment extends Fragment {
             Toast.makeText(requireContext(), "contiene", Toast.LENGTH_SHORT).show();
         }
     }
+
 }
