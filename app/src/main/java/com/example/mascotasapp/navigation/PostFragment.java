f62bba10e635fdd0991e777c4f9f1ba7250c799f
NicoHerlan <nicodvkcss@gmail.com>
2023-11-07 17:14:17 -0300

    Add map activity

--- app/src/main/java/com/example/mascotasapp/navigation/PostFragment.java
+++ app/src/main/java/com/example/mascotasapp/navigation/PostFragment.java
@@ -1,16 +1,29 @@
 package com.example.mascotasapp.navigation;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.mascotasapp.PostActivity;
 import com.example.mascotasapp.R;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.maps.CameraUpdate;
@@ -24,6 +37,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.chip.Chip;
 import com.google.firebase.firestore.GeoPoint;
 import com.squareup.picasso.Picasso;
+
 import java.util.Map;
 
 public class PostFragment extends Fragment implements OnMapReadyCallback {
@@ -36,6 +50,7 @@ public class PostFragment extends Fragment implements OnMapReadyCallback {
     GoogleMap map;
     private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
     Map<String, Object> post;
+
     public PostFragment(Map<String, Object> post, Context context, Activity activity) {
         this.post = post;
         this.context = context;
@@ -55,7 +70,7 @@ public class PostFragment extends Fragment implements OnMapReadyCallback {
         description = view.findViewById(R.id.FragPostTextDesc);
         category = view.findViewById(R.id.FragPostChipCategory);
 
-        mMapView =  view.findViewById(R.id.FragPostMap);
+        mMapView = view.findViewById(R.id.FragPostMap);
         mMapView.onCreate(savedInstanceState);
         mMapView.getMapAsync(this);
 
@@ -73,7 +88,21 @@ public class PostFragment extends Fragment implements OnMapReadyCallback {
         LatLng latLng = new LatLng(geo.getLatitude(), geo.getLongitude());
         map.addMarker(new MarkerOptions().position(latLng));
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 20));
+        provider(map);
     }
+
+    public void provider(GoogleMap googleMap) {
+        if (ActivityCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            if(ActivityCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                googleMap.setMyLocationEnabled(true);
+            }
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+        googleMap.setOnMyLocationClickListener(v -> Toast.makeText(requireContext(), "RR", Toast.LENGTH_SHORT).show());
+    }
+
 
     public void setData(){
 
@@ -94,5 +123,29 @@ public class PostFragment extends Fragment implements OnMapReadyCallback {
                 .into(postPhoto);
 
 
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
