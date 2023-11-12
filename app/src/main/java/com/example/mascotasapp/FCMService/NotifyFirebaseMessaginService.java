cee1dd69e90666d9477e6d3fe776e5d6f29383df
NicoHerlan <nicodvkcss@gmail.com>
2023-11-12 19:54:32 -0300

    Add notify activity

--- app/src/main/java/com/example/mascotasapp/FCMService/NotifyFirebaseMessaginService.java
+++ app/src/main/java/com/example/mascotasapp/FCMService/NotifyFirebaseMessaginService.java
@@ -6,16 +6,19 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Build;
 import android.util.Log;
-
+import android.widget.RemoteViews;
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
-
+import com.example.mascotasapp.NotifyActivity;
 import com.example.mascotasapp.R;
-import com.example.mascotasapp.navigation.MainActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
 
 @SuppressLint("MissingFirebaseInstanceTokenRefresh")
 public class NotifyFirebaseMessaginService extends FirebaseMessagingService {
@@ -24,26 +27,61 @@ public class NotifyFirebaseMessaginService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage){
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        Log.d(TAG, "Message data payload: " + remoteMessage.getData());
 
-            // Puedes acceder a los datos del mensaje usando remoteMessage.getData()
-            String message = remoteMessage.getData().get("message");
+        // Acceder a los datos del mensaje usando remoteMessage.getData()
+        String type = remoteMessage.getData().get("type");
+        String title = remoteMessage.getData().get("title");
+        String photo = remoteMessage.getData().get("photo");
+        String message = remoteMessage.getData().get("message");
+        String deviceId = remoteMessage.getData().get("deviceId");
+        String username = remoteMessage.getData().get("username");
 
-            // Muestra la notificación o realiza otras acciones según tus necesidades
-            showNotification(message);
+        // Muestra la notificación o realiza otras acciones según tus necesidades
+        showNotification(type, title, photo, message, deviceId, username);
 
     }
-    private void showNotification(String message) {
+
+    private void showNotification(String type, String title, String photo, String message, String deviceId, String username) {
         // Crear un Intent para la actividad principal de la aplicación
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        Intent intent = new Intent(this, NotifyActivity.class);
+        intent.putExtra("type", type);
+        intent.putExtra("title", title);
+        intent.putExtra("photoUrl", photo);
+        intent.putExtra("message", message);
+        intent.putExtra("deviceId", deviceId);
+        intent.putExtra("username", username);
+
+// Asegúrate de que la bandera PendingIntent.FLAG_UPDATE_CURRENT se establezca para actualizar los extras si la notificación se ha creado previamente
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_MUTABLE);
 
-        // Construir la notificación
+
+        // Crear un RemoteViews para la vista compacta
+        @SuppressLint("RemoteViewLayout") RemoteViews notificationLayoutCompact = new RemoteViews(getPackageName(), R.layout.notification_layout_compact);
+        notificationLayoutCompact.setTextViewText(R.id.notification_title, title);
+        notificationLayoutCompact.setTextViewText(R.id.notification_message, username + " " + message);
+
+        // Crear un RemoteViews para la vista expandida
+        @SuppressLint("RemoteViewLayout") RemoteViews notificationLayoutExpanded = new RemoteViews(getPackageName(), R.layout.notification_layout_expanded);
+        notificationLayoutExpanded.setTextViewText(R.id.notification_title, title);
+        notificationLayoutExpanded.setTextViewText(R.id.notification_message,username + " " + message);
+
+        // Cargar la imagen usando Picasso en ambas vistas
+        if (photo != null && !photo.isEmpty()) {
+            Bitmap bitmap = getBitmapFromUrl(photo);
+            if (bitmap != null) {
+                notificationLayoutCompact.setImageViewBitmap(R.id.notification_image, bitmap);
+                notificationLayoutExpanded.setImageViewBitmap(R.id.notification_image, bitmap);
+            } else {
+                Log.e(TAG, "Error al cargar la imagen desde la URL");
+            }
+        }
+
+        // Construir la notificación con las vistas compacta y expandida
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "channel_id")
                 .setSmallIcon(R.drawable.ic_notify)
-                .setContentTitle("Título de la notificación")
-                .setContentText(message)
+                .setCustomContentView(notificationLayoutCompact)
+                .setCustomBigContentView(notificationLayoutExpanded)
                 .setAutoCancel(true)
                 .setContentIntent(pendingIntent);
 
@@ -59,6 +97,19 @@ public class NotifyFirebaseMessaginService extends FirebaseMessagingService {
 
         // Mostrar la notificación
         notificationManager.notify(0, notificationBuilder.build());
+    }
+
+
+// Resto del código...
+
+
+    private Bitmap getBitmapFromUrl(String url) {
+        try {
+            return Picasso.with(this).load(url).resize(50, 50).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 
